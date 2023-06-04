35a3925cca40b95ffa94cd9ee129965e420bcf01
solzycoded <solomonfidelis012@gmail.com>
2023-06-04 10:11:36 +0100

    setting up login and register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\WishlistController;
 use App\Http\Controllers\ProductWishlistController;
+
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,8 +14,8 @@ use App\Http\Controllers\ProductWishlistController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -27,3 +29,21 @@ Route::post('/store-viewall-trigger', [ProductController::class, 'storeViewAllTr
 // WISHLIST
 Route::post('/add-to-wishlist', [WishlistController::class, 'update']); // create or delete a product, from wishlist
 Route::get('/show-wishlist', [ProductWishlistController::class, 'index']); // create or delete a product, from wishlist
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+// PROFILE
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// AUTH
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
