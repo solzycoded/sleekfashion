be8f8db353eae4c1afdb05ce95b6771a31034070
solzycoded <solomonfidelis012@gmail.com>
2023-07-25 19:16:37 +0100

    still working checkout page, DESIGN

--- resources/views/components/checkout/page.blade.php
+++ resources/views/components/checkout/page.blade.php
@@ -23,25 +23,71 @@
 
                             <div class="ml-1">
                                 {{-- door delivery --}}
-                                <div class="mb-1">
+                                <div class="mb-3">
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" name="checkout-delivery" id="checkout-delivery-door">
-                                        <label class="form-check-label" for="checkout-delivery-door">Door Delivery
-                                        </label>
+                                        <label class="form-check-label" for="checkout-delivery-door">Door Delivery</label>
                                     </div>
                                     {{-- details for door delivery --}}
                                     <div style="padding-left: 20px !important" class="pb-2">
-                                        something
+                                        {{-- add section for new address --}}
+                                        <div class="new-address">
+                                            <button class="btn btn-dark text-white">
+                                                Add new address <i class="bi bi-plus-circle-fill"></i>
+                                            </button>
+                                            <div class="mt-4">
+                                                <form action="/new-customer-address"
+                                                    method="POST">
+                                                    <input type="text" class="form-control p-1" name="new_address" id="new-address" placeholder="Type New Address here">
+
+                                                    <div style="text-align: right !important">
+                                                        <button class="btn btn-primary text-primary mt-1 bg-white pt-1" type="submit">
+                                                            Save <i class="bi bi-check-circle-fill"></i>
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="address-list mt-5">
+                                            <h5>
+                                                Available Address List 
+                                                <small>(You selected: <span class="text-black-50">No 8 obasuyi crescent, Ijegun road, Ikotun, Lagos</span>)</small>
+                                            </h5>
+                                            <ol class="list-group">
+                                                <li class="list-group-item align-items-start">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="radio" name="customer-address" checked>
+                                                        <label class="form-check-label text-muted">No 8 obasuyi crescent, Ijegun road, Ikotun, Lagos</label>
+                                                    </div>
+                                                </li>
+                                            </ol>
+                                            <div class="d-none"><p class="text-grey">You currently have no address listed yet.</p></div>
+                                        </div>
                                     </div>
                                 </div>
                                 {{-- pick up --}}
-                                {{-- <div>
+                                <div>
                                     <div class="form-check">
                                         <input class="form-check-input" type="radio" name="checkout-delivery" id="checkout-delivery-pickup">
                                         <label class="form-check-label" for="checkout-delivery-pickup">Pick Up
                                         </label>
                                     </div>
-                                </div> --}}
+                                    <div style="padding-left: 20px !important" class="pb-2">
+                                        <h5>
+                                            Pick a location that's closest you
+                                            <small>(You selected: <span class="text-black-50">No 8 obasuyi crescent, Ijegun road, Ikotun, Lagos</span>)</small>
+                                        </h5>
+                                        <ol class="list-group">
+                                            <li class="list-group-item align-items-start">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="available-pickup-location" checked>
+                                                    <label class="form-check-label text-muted">No 8 obasuyi crescent, Ijegun road, Ikotun, Lagos</label>
+                                                </div>
+                                            </li>
+                                        </ol>
+                                        <div class="d-none"><p class="text-grey">There are no available locations in your current vicinity / state.</p></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
